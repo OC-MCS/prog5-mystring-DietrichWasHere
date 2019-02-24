20a07b43bb6a3cbf523981c1abf239fdbce2cd56
DVersaw <dietrich.versaw@eagles.oc.edu>
2019-02-24 17:47:40 -0600

    Did this work

--- MyString/testMyString.cpp
+++ MyString/testMyString.cpp
@@ -1,12 +1,15 @@
 // test driver code goes here
 #include "MyString.h"
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 // test MyString class
 int main()
 {
-	MyString s1, s2, s3;
+	MyString s1; // test string 1
 	cout << "String 1:" << s1 << endl;
-
+	char cs1[11] = "aaaaaaaaaa"; // teste cstring 1
+	MyString s2(cs1); // test string 2
+	cout << s2;
 }
