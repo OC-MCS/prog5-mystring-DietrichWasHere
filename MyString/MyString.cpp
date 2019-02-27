9889bf358c3c4dd6e81c86a5522d274937b2508f
DVersaw <dietrich.versaw@eagles.oc.edu>
2019-02-27 11:40:41 -0600

    Finished

--- MyString/MyString.cpp
+++ MyString/MyString.cpp
@@ -7,43 +7,36 @@ using namespace std;
 MyString::MyString()
 {
 	str = nullptr;
-	cout << "con" << endl;
 }
 
 // Constructor that recieves c-string (char array)
 MyString::MyString(const char * nStr)
 {
-	cout << "con - char" << endl;
 	if (nStr != nullptr)
 	{
 		str = new char[strlen(nStr) + 1];
 		strcpy_s(str, strlen(nStr) + 1, nStr);
 	}
 	else nStr = nullptr;
-	cout << "con - char - end" << endl;
 }
 // copy constructor
 MyString::MyString(const MyString& oStr)
 {
-	cout << "copy con" << endl;
 	if (oStr.str != nullptr)
 	{
 		str = new char[strlen(oStr.str) + 1];
 		strcpy_s(str, strlen(oStr.str) + 1, oStr.str);
 	}
 	else str = nullptr;
-	cout << "copy con - end" << endl;
 }
 // destructor
 MyString::~MyString()
 {
-	cout << "destructor - end" << endl;
 	delete[] str;
 }
 // set one MyString to the value of another
 MyString MyString::operator = (const MyString &other)
 {
-	cout << "overloaded equal" << endl;
 	if ((this != &other) && (other.str != nullptr))
 	{
 		delete[] str;
@@ -51,13 +44,11 @@ MyString MyString::operator = (const MyString &other)
 		strcpy_s(str, strlen(other.str) + 1, other.str);
 	}
 	else if (other.str == nullptr) str = nullptr;
-	cout << "overloaded equal - end" << endl;
 	return other;
 }
 // concatenate two MyStrings, return the new MySttring
 MyString MyString::operator + (const MyString & other)
 {
-	cout << "+ operator" << endl;
 	MyString nStr;
 	if ((str != nullptr) && (other.str != nullptr))
 	{
@@ -76,16 +67,13 @@ MyString MyString::operator + (const MyString & other)
 		strcpy_s(nStr.str, strlen(str) + 1, str);
 	}
 	else nStr.str = nullptr;
-	cout << "+ operator - end" << endl;
 	return nStr;
 }
 // test if two myStrigns are equal, return bool indicating truth of satement
 bool MyString::operator == (const MyString & other)
 {
-	cout << "MyString compare" << endl;
 	bool equal = false;
 	if (strcmp(str, other.str)) equal = true;
-	cout << "MyString compare - end" << endl;
 	return equal;
 }
 // get the value of the c-string pointer, for overloaded << operator
@@ -96,8 +84,6 @@ char * MyString::c_str() const
 // use << operator to displat MyString's c-string
 ostream & operator << (ostream &strm, const MyString &out)
 {
-	cout << "display overload" << endl;
 	if (out.c_str() != nullptr) strm << out.c_str();
-	cout << "display overload - end" << endl;
 	return strm;
 }
